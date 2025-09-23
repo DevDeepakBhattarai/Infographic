8ad542bad76bbea57f760d5cf88cff4f88b131c2
Aaron <antvaaron@gmail.com>
2025-09-23 15:41:01 +0800

    feat(infographic): add item value component

---
+++ packages/infographic/src/designs/components/ItemValue.tsx
@@ -0,0 +1,36 @@
+/** @jsxImportSource @antv/infographic-jsx */
+import type { TextProps } from '@antv/infographic-jsx';
+import { Text } from '@antv/infographic-jsx';
+import { getItemKeyFromIndexes } from '../../utils';
+
+export interface ItemValueProps extends TextProps {
+  indexes: number[];
+  value: number;
+  formatter?: (value: number) => string | number;
+}
+
+export const ItemValue = ({
+  indexes,
+  value,
+  formatter = (v) => String(v),
+  ...props
+}: ItemValueProps) => {
+  const finalProps: TextProps = {
+    width: 100,
+    fontSize: 14,
+    fill: '#666',
+    wordWrap: true,
+    lineHeight: 1.4,
+    children: formatter(value),
+    'data-value': value,
+    ...props,
+  };
+
+  finalProps.height ??= Math.ceil(
+    +finalProps.lineHeight! * +finalProps.fontSize!,
+  );
+
+  return (
+    <Text {...finalProps} id={`item-${getItemKeyFromIndexes(indexes)}-value`} />
+  );
+};
